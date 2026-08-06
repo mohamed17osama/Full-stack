394dcadaffab38d0b3ecda2138e9bf915419adf3
mohamed17osama <mohamedtriton687@gmail.com>
2026-08-06 04:07:48 +0300

    Add files via upload

---
+++ Bank system/Bank system/models/BankAccount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank_system.models
+{
+    internal class BankAccount
+    {
+        private decimal _balance;
+
+        public string Owner;
+        public decimal Balance
+        {
+            get { return _balance; }
+        }
+        public BankAccount(string owner, decimal balance)
+        {
+            Owner = owner;
+
+        }
+        public void Deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Denied");
+            }
+            else
+            {
+                _balance += amount;
+
+                Console.WriteLine("Accepted");
+            }
+      
+        }
+        public void Withdraw(decimal amount)
+        {
+            if (_balance < amount && amount <= 0)
+            {
+                Console.WriteLine("Denied");
+            }
+            else
+            {
+                _balance -= amount;
+
+                Console.WriteLine("Accepted");
+            }
+
+        }
+
+        public virtual string GetAccountType()
+        {
+            return "Standard";
+        }
+
+    }
+}
