4ca9fb614198e65eaf0065e43d793485355afc1e
mohamed17osama <mohamedtriton687@gmail.com>
2026-08-06 05:16:09 +0300

    Add files via upload

---
+++ Shape rendering system/Shape rendering system/models/IDrawable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_rendering_system.models
+{
+    internal interface IDrawable
+    {
+        void Draw();
+    }
+}
---
+++ Shape rendering system/Shape rendering system/models/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shape_rendering_system.models
+{
+    internal class Triangle : Shape, IDrawable
+    {
+        public Triangle(double area) : base(area) { }
+        public override double Area()
+        {
+            return base.area;
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("   **");
+            Console.WriteLine("  ****");
+            Console.WriteLine(" ******");
+            Console.WriteLine("********");
+        }
+    }
+}
